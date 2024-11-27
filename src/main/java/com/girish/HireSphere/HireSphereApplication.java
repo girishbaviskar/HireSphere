ba11e8947d9d1d774d8c734f695260eb82231876
Girish Baviskar <gbaviskar@umass.edu>
2024-11-27 13:28:09 -0500

    first commit

---
+++ src/main/java/com/girish/HireSphere/HireSphereApplication.java
@@ -0,0 +1,13 @@
+package com.girish.HireSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/girish/HireSphere/HireSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.girish.HireSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
